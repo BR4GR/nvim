a4ff2b20e8a2519b0555b3bd0391186ec4e263ca
benjamin wuermli <benjamin.wuermli@bertschi.com>
2025-06-17 17:49:35 +0200

    update php js settings

---
+++ after/ftplugin/javascript.lua
@@ -0,0 +1,7 @@
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.expandtab = true
+
+vim.opt_local.cindent = true
+vim.opt_local.cinoptions = ">4,n-2,{2,^-2,:2,=2,g0,h2,p5,t0,+2,(0,u0,w1,m1"
--- lua/plugins/php.lua
+++ lua/plugins/php.lua
@@ -3,27 +3,32 @@ return {
     {
         "neovim/nvim-lspconfig",
         opts = {
+            -- @type lspconfig.options
             servers = {
-                -- Configure intelephense
                 intelephense = {
-                    -- Basic settings
+                    filetypes = { "php", "blade", "php_only" },
                     settings = {
                         intelephense = {
-                            -- Add any specific intelephense settings here
+                            filetypes = { "php", "blade", "php_only" },
+                            files = {
+                                associations = { "*.php", "*.blade.php" }, -- Associating .blade.php files as well
+                                maxSize = 5000000,
+                            },
                         },
                     },
                 },
-                -- Add Psalm LSP
-                -- psalm = {},
             },
         },
     },
 
+    -- Configure formatter using conform.nvim (new approach)
     {
         "stevearc/conform.nvim",
         opts = {
             formatters_by_ft = {
                 php = { "phpcbf" },
+                javascript = { "eslint_d" },
+                html = { "htmlhint" },
             },
             formatters = {
                 phpcbf = {
@@ -45,6 +50,7 @@ return {
         opts = {
             linters_by_ft = {
                 php = { "phpcs" },
+                -- html = { "htmlhint" },
             },
             linters = {
                 phpcs = {
@@ -55,7 +61,7 @@ return {
                         "-",
                     },
                     ignore_exitcode = true,
-                    prepend_args = function(args, ctx)
+                    prepend_args = function(_, ctx)
                         -- Try to use local version if available
                         local local_phpcs =
                             vim.fs.find({ "vendor/bin/phpcs" }, { path = ctx.filename, upward = true })[1]
