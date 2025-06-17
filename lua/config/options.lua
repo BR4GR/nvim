467fbbae56a33bd03feabcdc3844bb4563b07706
benjamin wuermli <benjamin.wuermli@bertschi.com>
2025-06-17 17:50:10 +0200

    x

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,6 +10,14 @@ vim.opt_local.expandtab = true
 vim.opt_local.autoindent = true
 vim.opt_local.smarttab = true
 
+-- Example configuration using a list of dictionaries:
+vim.g.dbs = {
+    { name = "local", url = "mysql://root:R9TctcLLpdzwbM4Kxvcz@extranetlocal.bertschi.com:3307" },
+    -- { name = "staging", url = "Replace with your database connection URL." },
+}
+
+vim.g.dadbod_default_connection = vim.g.dbs[1].url
+
 if vim.env.SSH_CONNECTION then
     local function vim_paste()
         local content = vim.fn.getreg('"')
@@ -28,4 +36,3 @@ if vim.env.SSH_CONNECTION then
         },
     }
 end
-
--- lua/plugins/copilotchat.lua
+++ lua/plugins/copilotchat.lua
@@ -2,7 +2,7 @@ return {
     {
         "CopilotC-Nvim/CopilotChat.nvim",
         dependencies = {
-            { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+            { "zbirenbaum/copilot.lua" }, -- or zbirenbaum/copilot.lua
             { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
         },
         build = "make tiktoken", -- Only on MacOS or Linux
@@ -14,9 +14,13 @@ return {
                 title = "dahole",
             },
 
+            highlight_headers = false,
+            error_header = "> [!ERROR] Error",
+            separator = "---",
+
             auto_follow_cursor = false,
             auto_insert_mode = false,
-            chat_autocomplete = true,
+            chat_autocomplete = false,
             question_header = "# batman ",
             answer_header = "# robin ",
 
@@ -27,7 +31,7 @@ return {
                 },
             },
             contexts = {
-                buffers = { "listed" },
+                buffer = { "listed" },
             },
         },
         -- See Commands section for default commands if you want to lazy load on them
